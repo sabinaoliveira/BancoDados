13a1b39cd81c4a409847af9f339e00bafbbe7759
Sabina <104401758+sabinaoliveira@users.noreply.github.com>
2024-08-06 23:25:57 +0000

    a

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,22 @@
+# print() mostra o argumento na tela&nbsp;
+print ("Oi")
+
+# len() determina a quantidade de caracteres em uma cadeia deles.
+len("Oi Python")
+
+# range() cria um intervalo de números.
+# list() cria uma lista a partir de um elemento.
 x = range (5)
 print (list(x))
+
+# max() determina o valor máximo entre um grupo de números.
+x = [0, 1, 2]
+print (max(x))
+
+# min() determina o valor mínimo entre um grupo de números.
+x = [0, 1, 2]
+print (min(x))
+
+# sum() soma o total de uma lista de números.
+x = [0, 1, 2]
+print (sum(x))
