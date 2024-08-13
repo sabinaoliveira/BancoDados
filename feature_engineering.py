0a3ff5001c1405284b6207249ebfcc1d74cca868
Sabina <104401758+sabinaoliveira@users.noreply.github.com>
2024-08-13 14:33:31 +0000

    a

--- feature_engineering.py
+++ feature_engineering.py
@@ -8,6 +8,22 @@ def preencher_nivel(gestor,nivel):
 
 dados["NOVO_NIVEL"] = dados.apply(lambda x: preencher_nivel(x["GESTOR"], x["NIVEL"]), axis=1)
 
+dados["NOVO_NIVEL"].value_counts()
+
+dados = pd.get_dummies(dados, columns=["NIVEL"])
+
+def determinar_geracao(idade):
+  if 39<idade<58:
+    return "Geração X"
+  elif 29<idade <=39:
+    return "MilLenial"
+  elif 13<idade <=29:
+    return "Geração Z"
+  else:
+    return "Outra geração"
+
+  
+
 
 
     
